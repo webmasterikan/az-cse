9393ef263d83c7f5cddc7bcd4cadb014649a192e
webmasterikan <webmaster@ikan.be>
2022-07-04 16:28:36 +0200

    Create init.ps1

---
+++ init.ps1
@@ -0,0 +1,22 @@
+Set-ExecutionPolicy Unrestricted
+
+
+<#
+--- Run ikanalm setup script as user not system account
+#>
+
+$username = $Args[0]
+$password = $Args[1]
+
+
+#$password =  ConvertTo-SecureString "[your admin account user password]" -AsPlainText -Force
+#$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\[your admin account]", $password)
+#$command = $file = $PSScriptRoot + "\CustomScriptSQLPS.ps1"
+
+$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$username", $password)
+
+$command = "ikanalm-setup.ps1"
+
+Enable-PSRemoting –force
+Invoke-Command -FilePath $command -Credential $credential -ComputerName $env:COMPUTERNAME
+Disable-PSRemoting -Force
