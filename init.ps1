93748ff503d9ba1e4b4b6eecbd403df1458902ad
webmasterikan <webmaster@ikan.be>
2022-07-05 16:11:04 +0200

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -4,8 +4,8 @@ Set-ExecutionPolicy Unrestricted
 <#
 --- Run ikanalm setup script as user not system account
 #>
-$myUsername = $Args[0]
-$Mypassword = $Args[1]
+$myUsername = ikanalmadmin
+$Mypassword = trAT2IreCGpt7suqzJVZ
 $machineName = $env:COMPUTERNAME
 
 $username = $env:COMPUTERNAME+'\'+$myUsername
@@ -17,10 +17,11 @@ $password =  ConvertTo-SecureString $Mypassword -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential($username, $password)
 
 
-$command = "https://raw.githubusercontent.com/webmasterikan/az-cse/main/NOikanalm-setup.ps1"
+$command = "https://raw.githubusercontent.com/webmasterikan/az-cse/main/ikanalm-setup.ps1"
 
 
 Enable-PSRemoting -Force
 Invoke-Command -FilePath $command -Credential $credential -ComputerName $machineName
 Disable-PSRemoting -Force
 
+stop-process -Id $PID
