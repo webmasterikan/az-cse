b412d561fd4fb6fa49076879847beaa298e5a380
webmasterikan <webmaster@ikan.be>
2022-07-05 16:40:11 +0200

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -10,14 +10,17 @@ $machineName = $env:COMPUTERNAME
 
 $username = $env:COMPUTERNAME+'\'+$myUsername
 
-
+Set-Content -Path 'C:\temp\testfile.txt' -Value 'CSE test1'
+Set-Content -Path 'C:\temp\testuser.txt' -Value $username
+Set-Content -Path 'C:\temp\testpass.txt' -Value $Mypassword
 
 $password =  ConvertTo-SecureString $Mypassword -AsPlainText -Force
 
 $credential = New-Object System.Management.Automation.PSCredential($username, $password)
 
 
-$command = "https://raw.githubusercontent.com/webmasterikan/az-cse/main/ikanalm-setup.ps1"
+#$command = "https://raw.githubusercontent.com/webmasterikan/az-cse/main/ikanalm-setup.ps1"
+$command = "C:\temp\run-ikanalm-init.ps1"
 
 
 Enable-PSRemoting -Force
