5dda4703380aeea56957bd641d4c64190086f25f
webmasterikan <webmaster@ikan.be>
2022-07-06 12:30:03 +0200

    Update ikanalm-setup.ps1

--- ikanalm-setup.ps1
+++ ikanalm-setup.ps1
@@ -11,12 +11,17 @@ Set-Content -Path 'C:\temp\file.txt' -Value 'CSE test string'
 Set-Content -Path 'C:\temp\Isthereanarg0.txt' -Value $Args[0]
 Set-Content -Path 'C:\temp\Isthereanarg1.txt' -Value $Args[1]
 
-Set-Content -Path 'C:\temp\Istherecredential.txt' -Value $credential
 
-$testcred = $Args[0]
+$myUsername = $Args[0]
+$Mypassword = $Args[1]
 
-Start-Process net -arg "start Tomcat9" Credential $testcred
-Start-Process net -arg "start almsvr59" Credential $testcred
+$password =  ConvertTo-SecureString $Mypassword -AsPlainText -Force
+
+$credential = New-Object System.Management.Automation.PSCredential($myUsername, $password)
+
+Start-Process net -arg "start Tomcat9" Credential $credential
+Start-Process net -arg "start almsvr59" Credential $credential
+
 
 
 
