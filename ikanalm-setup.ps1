38f8c9858435a75364f795c90de9d9d27c325818
webmasterikan <webmaster@ikan.be>
2022-07-06 13:31:38 +0200

    Update ikanalm-setup.ps1

--- ikanalm-setup.ps1
+++ ikanalm-setup.ps1
@@ -19,8 +19,8 @@ $password =  ConvertTo-SecureString $Mypassword -AsPlainText -Force
 
 $credential = New-Object System.Management.Automation.PSCredential($myUsername, $password)
 
-Start-Process net -arg "start Tomcat9" Credential $credential
-Start-Process net -arg "start almsvr59" Credential $credential
+Start-Process net -arg "start Tomcat9" -Credential $credential
+Start-Process net -arg "start almsvr59" -Credential $credential
 
 
 
