491ed5dccc8cc5b8f5a4c6c1384ad9478ead7009
webmasterikan <webmaster@ikan.be>
2022-07-06 12:02:30 +0200

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -30,7 +30,7 @@ $credential = New-Object System.Management.Automation.PSCredential($username, $p
 $command = $PSScriptRoot + "\ikanalm-setup.ps1"
 
 
-Invoke-Command -FilePath $command -Credential $credential -ComputerName $machineName
+Invoke-Command -FilePath $command -Credential $credential -ComputerName $machineName -ArgumentList $credential
 
 
 
