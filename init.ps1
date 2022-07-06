b61890a2288859b7c4423fbdd840532a771f68f6
webmasterikan <webmaster@ikan.be>
2022-07-06 13:59:24 +0200

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -8,20 +8,11 @@ $myUsername = $Args[0]
 $Mypassword = $Args[1]
 $machineName = $env:COMPUTERNAME
 
-Set-Content -Path 'C:\temp\arg0.txt' -Value $Args[0]
-Set-Content -Path 'C:\temp\arg1.txt' -Value $Args[1]
-Set-Content -Path 'C:\temp\arg2.txt' -Value $Args[2]
-Set-Content -Path 'C:\temp\arg3.txt' -Value $Args[3]
+
 
 $username = $env:COMPUTERNAME+'\'+$myUsername
 
-Set-Content -Path 'C:\temp\testfile.txt' -Value 'CSE test1'
-Set-Content -Path 'C:\temp\testuser.txt' -Value $username
-Set-Content -Path 'C:\temp\testpass.txt' -Value $Mypassword
 
-
-Set-Content -Path 'C:\temp\filevar.txt' -Value $file
-Set-Content -Path 'C:\temp\psscriptrootvar.txt' -Value $PSScriptRoot
 
 $password =  ConvertTo-SecureString $Mypassword -AsPlainText -Force
 
